3fd3fc3cb8df950c88f17999265ad71fb655a8dc
Davi Castro <santos.davi2110@gmail.com>
2026-05-05 16:54:04 -0300

    fix(rest-day-card): remove lowercase to display weekday in uppercase

--- src/app/home/components/rest-day-card.tsx
+++ src/app/home/components/rest-day-card.tsx
@@ -10,7 +10,7 @@ export function RestDayCard({ weekday }: IRestDayCardProps) {
   return (
     <div className="flex h-[110px] w-full flex-col items-center justify-between rounded-xl bg-[#f1f1f1] p-5">
       <div className="flex items-center justify-center">
-        <span className="flex items-center gap-1 rounded-full bg-[#00000014] px-[10px] py-[5px] text-[12px] font-semibold text-black lowercase">
+        <span className="flex items-center gap-1 rounded-full bg-[#00000014] px-[10px] py-[5px] text-[12px] font-semibold text-black">
           <Calendar className="h-3.5 w-3.5" />
           <span>{weekday}</span>
         </span>
