733f275d08cc1f0e2739000d3c4d9e8eb46a4a02
Davi Castro <santos.davi2110@gmail.com>
2026-05-12 19:25:05 -0300

    chore(next-config): add allowedDevOrigins to next configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['better-auth'],
+  allowedDevOrigins: ['192.168.0.8'],
 };
 
 export default nextConfig;
